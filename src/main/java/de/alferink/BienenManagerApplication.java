054cbf977d73a04567ec0ee4972082b6d24b1768
alferink <joerg.alferink@web.de>
2016-10-10 21:20:04 +0200

    Initial commit

---
+++ src/main/java/de/alferink/BienenManagerApplication.java
@@ -0,0 +1,12 @@
+package de.alferink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BienenManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BienenManagerApplication.class, args);
+	}
+}
